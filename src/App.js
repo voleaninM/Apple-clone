c5bcfa2ed18f7524b01ee53b52d0ea5b0fc8ab67
Maxim <voleaninmax@gmail.com>
2022-11-28 12:14:52 +0200

    resize

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
   const location = useLocation()
   return (
     <AnimatePresence
-    exitBeforeEnter
+    wait
     >
       <Routes location={location} key={location.pathname}>
           <Route path='/' element={<Home/>}/>
